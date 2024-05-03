7868e7c375abbdde778a97192f90500975e67035
Sturek <Sturek@users.noreply.github.com>
2024-05-03 14:22:08 +0200

    missing comma

--- users/sturek/macros.c
+++ users/sturek/macros.c
@@ -1,22 +1,10 @@
 // Define all of
 enum custom_keycodes {
-  KC_ETREMA = SAFE_RANGE
+  KC_ETREMA = SAFE_RANGE,
   KC_NDASH,
   KC_MDASH,
   NEW_SAFE_RANGE  //use "NEW_SAFE_RANGE" for keymap specific codes
 };
-
-// Left-hand home row mods
-#define HOME_A LGUI_T(KC_A)
-#define HOME_R LT(4, KC_R)
-#define HOME_S LSFT_T(KC_S)
-#define HOME_T LCTL_T(KC_T)
-
-// Right-hand home row mods
-#define HOME_N RCTL_T(KC_N)
-#define HOME_E RSFT_T(KC_E)
-#define HOME_I LT(4, KC_I)
-#define HOME_O RGUI_T(KC_O)
 
 __attribute__ ((weak))
 bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
