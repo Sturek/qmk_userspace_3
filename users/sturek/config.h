62c4f4f2315078c403f598f12a106a69cdbdfc22
Sturek <Sturek@users.noreply.github.com>
2024-05-03 14:19:03 +0200

    moved hrm back to config.h

--- users/sturek/config.h
+++ users/sturek/config.h
@@ -8,6 +8,18 @@
 #define TAPPING_FORCE_HOLD
 #define TAP_CODE_DELAY 10
 
+// Left-hand home row mods
+#define HOME_A LGUI_T(KC_A)
+#define HOME_R LT(4, KC_R)
+#define HOME_S LSFT_T(KC_S)
+#define HOME_T LCTL_T(KC_T)
+
+// Right-hand home row mods
+#define HOME_N RCTL_T(KC_N)
+#define HOME_E RSFT_T(KC_E)
+#define HOME_I LT(4, KC_I)
+#define HOME_O RGUI_T(KC_O)
+
 #ifdef COMBO_ENABLE
 # define COMBO_COUNT 8
 # include "combos.c"
